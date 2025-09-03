9a4a85b08efe781eeff24541b062e82424217b10
Daniel Alecrim <danielalecrim.tvc@gmail.com>
2025-09-02 23:42:26 -0300

    Adicionando métodos excluir , ler e localizar da classe VetorTAD

--- datastructure/src/TAD/VETOREXEMPLO/VetorTAD.java
+++ datastructure/src/TAD/VETOREXEMPLO/VetorTAD.java
@@ -108,5 +108,86 @@ public class VetorTAD {
 		return 0;
 	}
 	
+	/**
+    *
+    * <br> ================================================================
+    * <br> EXCLUI O VALOR DE DETERMINADA POSICAO
+    * <br> ================================================================
+    * <br> Retorna os seguintes codigos:
+    * <br> 0 - exclusao bem sucedida
+    * <br> 2 - posicao nao existente no vetor
+    * <br> 3 - posicao ja' esta' vaga (para excluir e' necessario estar
+    * alocada)
+    *
+    * @param posicao
+    * @return
+    */
 	
+	public int excluir(int posicao) {
+		if(!this.posicaoValida(posicao)) { // posicao válida ?
+			return 2;
+		}
+		
+		if (this.dados[posicao] == this.vaga) { // posicao contém um valor válido ?
+			return 3;
+	   }
+		
+	   this.dados[posicao] = this.vaga;
+	   
+	   return 0;
+	
+}
+	
+	/**
+     * <br> ====================================================================
+     * <br> L&Ecirc; UMA POSI&Ccedil;&Atilde;O NO VETOR
+     * <br> ====================================================================
+     *
+     * @param posicao
+     * @return O valor contido na posicao; se a posicao estiver vaga, retorna o
+     * valor que indica posicao vaga.
+     */
+	public int ler(int posicao) {
+		if(!this.posicaoValida(posicao)) { // posição válida?
+			return 2;
+		}
+		
+		return this.dados[posicao];
+	}
+	
+	/**
+     * <br> Localiza um determinado valor no vetor.
+     *
+     * @param valor Valor a ser localizado
+     * @param nPrimeiros Contabilizar apenas o "n" primeiros; 0 = contabiliza
+     * todos
+     * @return Um vetor com:
+     * <li> posicao 0 = quantidade de elementos encontrados
+     * <li> demais posicoes = indices desses elementos no vetor
+     */
+	
+	public int[] localizar(int valor,int nPrimeiros) {
+		
+		int[] res = new int[this.tamanho];
+		
+		if(!this.valorValido(valor)) {
+			return res;
+		}
+		
+		if(this.repete == 0) {
+			nPrimeiros = 1;
+		}
+		
+		for(int i = 0; i < this.tamanho; i++) {
+			if (this.dados[i] == valor) {
+				res[o]++;
+				res[res[0]] = i;
+				if(res[0] == nPrimeiros) {
+					break;
+				}
+			}
+		}
+		
+		return res;
+	}
 }
